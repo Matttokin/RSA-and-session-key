69b2681a488e0b781b9ac41a77d4be2f6425c8f8
Matttokin <55289178+Matttokin@users.noreply.github.com>
2020-04-08 21:06:00 +0300

    Initial commit

---
+++ RSA and session key/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSA_and_session_key
+{
+    public partial class Form1 : Form
+    {
+        BigInteger[] basicKey;
+        BigInteger[] sessionKey;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private BigInteger[] genKey(int p, int q)
+        {
+                BigInteger n = p * q;
+                BigInteger fn = (p - 1) * (q - 1);
+                BigInteger e = primeValue.genPrime((int)(fn/2)); // деление на 2 используем, чтобы не брать слишком большое E
+                BigInteger d = modInverse(e, fn);
+
+            return new BigInteger[3] { e, n, d };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int p = Convert.ToInt32(textBox1.Text);
+                int q = Convert.ToInt32(textBox2.Text);
+                basicKey = genKey(p, q);
+                sessionKey = genKey(primeValue.genPrime(new Random().Next(3000, 5000)), primeValue.genPrime(new Random().Next(5000, 8000)));
+
+                textBox3.Text = basicKey[0].ToString();
+                textBox4.Text = basicKey[1].ToString();
+                textBox5.Text = basicKey[2].ToString();
+                textBox6.Text = basicKey[1].ToString();
+                               
+                textBox7.Text =  encrypt(sessionKey[0].ToString(), (int)basicKey[0], (int)basicKey[1]).ToString();
+                textBox17.Text = encrypt(sessionKey[1].ToString(), (int)basicKey[0], (int)basicKey[1]).ToString();
+                textBox10.Text = encrypt(sessionKey[2].ToString(), (int)basicKey[0], (int)basicKey[1]).ToString();
+                textBox18.Text = encrypt(sessionKey[1].ToString(), (int)basicKey[0], (int)basicKey[1]).ToString();
+
+                string s1 = encrypt(sessionKey[0].ToString(), (int)basicKey[0], (int)basicKey[1]).ToString();
+                string s2 = decrypt(s1, (int) basicKey[2], (int)basicKey[1]).ToString();
+
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+        BigInteger modInverse(BigInteger a, BigInteger n)  //инверсия в кольце по модулю, взято и переделано с https://stackoverflow.com/questions/7483706/c-sharp-modinverse-function
+        {
+            BigInteger i = n, v = 0, d = 1;
+            while (a > 0)
+            {
+                BigInteger t = i / a, x = a;
+                a = i % x;
+                i = x;
+                x = d;
+                d = v - t * x;
+                v = x;
+            }
+            v %= n;
+            if (v < 0) v = (v + n) % n;
+            return v;
+        }
+        private BigInteger crypt(BigInteger m,int ed, int n) //либо E, либо D
+        {
+            return BigInteger.ModPow(m, ed, n);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BigInteger sessionE = BigInteger.Parse(decrypt(textBox8.Text, (int)basicKey[2], (int)basicKey[1]));
+                BigInteger sessionN = BigInteger.Parse(decrypt(textBox11.Text, (int)basicKey[2], (int)basicKey[1]));
+                textBox12.Text = encrypt(textBox9.Text, (int)sessionE, (int)sessionN);
+
+            } catch
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BigInteger sessionD = BigInteger.Parse(decrypt(textBox13.Text, (int)basicKey[2], (int)basicKey[1]));
+                BigInteger sessionN = BigInteger.Parse(decrypt(textBox14.Text, (int)basicKey[2], (int)basicKey[1]));
+
+                textBox16.Text = decrypt(textBox15.Text, (int)sessionD, (int)sessionN);
+
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+
+        private string encrypt(string text,int e,int n)
+        {
+            string outText = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                outText += crypt(BigInteger.Parse(((int)text[i]).ToString()), e, n).ToString().PadLeft(11, '0');
+            }
+            return outText;
+        }
+        private string decrypt(string text,int d,int n)
+        {
+            string outText = "";
+            for (int i = 0; i < text.Length / 11; i++)
+            {
+                outText += (char)crypt(BigInteger.Parse(text.Substring(i * 11, 11)), d, n);
+            }
+            return outText;
+        }   
+    }
+}
---
+++ RSA and session key/primeValue.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSA_and_session_key
+{
+    //решето аткина https://www.cyberforum.ru/csharp-beginners/thread176749.html
+    public static class primeValue
+    {
+        public static int genPrime(int limit)
+        {
+            bool[] is_prime = new bool[limit+1];
+            int n;
+
+            // Инициализация решета
+            int sqr_lim = (int)Math.Sqrt(limit);
+            for (int i = 0; i <= limit; i++) is_prime[i] = false;
+            is_prime[2] = true;
+            is_prime[3] = true;
+
+            // Предположительно простые - это целые с нечетным числом
+            // представлений в данных квадратных формах.
+            // x2 и y2 - это квадраты i и j (оптимизация).
+            int x2 = 0;
+            for (int i = 1; i <= sqr_lim; i++)
+            {
+                x2 += 2 * i - 1;
+                int y2 = 0;
+                for (int j = 1; j <= sqr_lim; j++)
+                {
+                    y2 += 2 * j - 1;
+
+                    n = 4 * x2 + y2;
+                    if ((n <= limit) && (n % 12 == 1 || n % 12 == 5))
+                        is_prime[n] = !is_prime[n];
+
+                    // n = 3 * x2 + y2; 
+                    n -= x2; // Оптимизация
+                    if ((n <= limit) && (n % 12 == 7))
+                        is_prime[n] = !is_prime[n];
+
+                    // n = 3 * x2 - y2;
+                    n -= 2 * y2; // Оптимизация
+                    if ((i > j) && (n <= limit) && (n % 12 == 11))
+                        is_prime[n] = !is_prime[n];
+                }
+            }
+
+            // Отсеиваем квадраты простых чисел в интервале [5, sqrt(limit)].
+            // (основной этап не может их отсеять)
+            for (int i = 5; i <= sqr_lim; i++)
+            {
+                if (is_prime[i])
+                {
+                    n = i * i;
+                    for (int j = n; j <= limit; j += n)
+                    {
+                        is_prime[j] = false;
+                    }
+                }
+            }
+
+            //модификация под RSA
+            int max = 1; 
+            for (int i = 2; i <= limit; i++)
+            {
+                if ((is_prime[i]))
+                {
+                    if(i>max && i != limit)
+                    {
+                        max = i;
+                    }
+                }
+            }
+            return max;
+        }
+    }
+}
